af8e35399aa26ec7995c1d74f1c10ba32c9b21e1
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-11-07 15:00:47 +0900

    feat: added google login with supabase

---
+++ pages/utilities/constants.ts
@@ -0,0 +1,6 @@
+import { SupabaseClient } from '@supabase/supabase-js'
+
+export const supabase = new SupabaseClient(
+  'https://nlbsnpoablmsxwkdbmer.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTE5ODEwMiwiZXhwIjoxOTQ0Nzc0MTAyfQ.XZWLzz95pyU9msumQNsZKNBXfyss-g214iTVAwyQLPA'
+)
